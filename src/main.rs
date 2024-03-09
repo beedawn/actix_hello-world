c7f548324d9dfbe52ce842ece1da0c494cda89ea
Bee Schmersal <brandonschmersal@gmail.com>
2024-03-09 07:40:41 -0500

    working on breaking up file read so it works primary with vectors

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,6 @@ let entry_path_string = entry_path.display().to_string();
     path_string
 }
 
-
 fn read_serve_files_as_bytes (user_path:String) -> Vec<u8> {
         let result:Vec<u8>=vec![];
         let file = fs::read(user_path);
@@ -46,9 +45,96 @@ fn read_serve_files_as_bytes (user_path:String) -> Vec<u8> {
         match file{
             Ok(file) => file,
             Err(err) => (format!("File not found. Error:{}",err).into())
-
     }
 }
+
+fn read_files_string (user_path:String)->String{
+    let mut path_string:String = "".to_owned();
+    
+    for entry in fs::read_dir(user_path.clone()).unwrap() {
+
+    let entry_path = entry.unwrap().path();
+    let entry_path_string = entry_path.display().to_string();
+        if let Ok(entry) = fs::read_dir(user_path.clone()) {
+            if Path::new(&entry_path_string).is_dir() {
+                path_string.push_str(read_files_string(entry_path_string.clone()).as_str());
+
+            }
+        }else{
+            println!("Error reading file directory");
+
+        }
+        path_string.push_str(entry_path_string.as_str());
+    }
+
+path_string
+
+}
+
+
+//the goal of this function is to take a string which is a path,
+//then function then searchs for each file and directory recursively
+// and converts their paths into a vector of PathBuf
+
+//maybe need to rethink the logic and extract the string reading process from read_files into it's
+//own function so it can be used in read_files and here.
+fn read_files_vec (user_path_vec:Vec<String>)->Vec<String>{
+    //so we need to loop over each string in the vector
+    //find all the files and directories, collect those, and put them into the same vector
+
+
+
+
+    //mutable string to build over course of function
+    let mut path_string:String = "".to_owned();
+//do we need a veector? yes here we do
+    let mut path_vector: Vec<PathBuf>= vec![];
+    //loops through each of the files 
+    //throws error if file is wrong
+    //
+    for single_path in user_path_vec{
+    for entry in fs::read_dir(single_path.clone()).unwrap() {
+        //unwraps entry into the path
+        let entry_path = entry.unwrap().path();
+        //gets a usable string from entry path because we use it alot right now
+let entry_path_string = entry_path.display().to_string();
+//if entry read is ok ?
+        if let Ok(entry) = fs::read_dir(single_path.clone()){
+           // println!("{:?}",entry_path);
+            // checks if entry is a directory
+            if Path::new(&entry_path_string).is_dir(){
+                //prints out found directories
+                println!("{} is dir", entry_path_string);
+                //recursively calls read_files
+                //need to figureout if we should move to vector or keep string 
+              
+                //throws compile error, need to figure out 
+                
+                // path_string.push_str(read_files_vec(entry_path_string.clone()).as_str());
+                path_vector.push(entry_path.clone());
+            }
+           // path_vector.push(entry_path.clone());
+        }else{
+            println!("Error reading file directory.");
+        }
+        path_string.push_str(format!("<p><a href=\"{}\">{}</a></p>\n",entry_path_string,entry_path_string).as_str());
+    }
+    }
+    println!("{:?}",path_vector);
+
+    //this is just here to get this to compile, the vec part
+    vec![path_string]
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -73,6 +159,7 @@ async fn chicken() -> impl Responder {
         Err(err) => HttpResponse::Ok().body(format!("Image not found, {}", err))
     }
 }
+
 //test of error handling if file exists/did not exist
 #[get("/gremlin")]
 async fn gremlin() -> impl Responder {
@@ -85,15 +172,15 @@ async fn gremlin() -> impl Responder {
     //check if file read of html variable(index.html) was successful
     match html{
         //no error, index.html path exists
-            Ok(html) => HttpResponse::Ok().content_type("text/html").body(html),
+        Ok(html) => HttpResponse::Ok().content_type("text/html").body(html),
         //if index does not exist, go check if 404 page exists
-     Err(err) => match error_page {
+        Err(err) => match error_page {
             //sends 404 error page
             Ok(error_page) => HttpResponse::Ok().content_type("text/html").body(error_page),
             //text displayed if both 404 page and index cannot be found
             Err(err)=> HttpResponse::Ok().body(error_var),
         }
-     }
+    }
 }
 
 //echos post request
@@ -139,7 +226,8 @@ println!("{}",path);
     HttpResponse::Ok().body(bytes)
 
 }
-//modular 
+//modular route configuration,
+//do we need a loop here to create all the end points?
 fn config(cfg: &mut web::ServiceConfig) {
 
     //need to write a loop here that gets the file names and then creates an end point and
@@ -147,15 +235,14 @@ fn config(cfg: &mut web::ServiceConfig) {
     //
     let mut x = 5;
     while x > 0 {
-    cfg.service(web::resource(format!("/{}",x))
+
+    cfg.service(web::resource(format!("/pizza/{}",x))
         .route(web::get().to(pizza_time))
         .route(web::head().to(|| HttpResponse::MethodNotAllowed()))
     );
     x-=1;
     }
 }
-
-
 
 //main server
 #[actix_web::main]
